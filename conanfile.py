1ac3c9b8a9d1579c247096b2ad42fdc616aec95e
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:32 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,22 +3,22 @@ from conans import ConanFile
 
 class BoostInterprocessConan(ConanFile):
     name = "Boost.Interprocess"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Container/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Date_Time/1.65.1@bincrafters/testing", \
-        "Boost.Integer/1.65.1@bincrafters/testing", \
-        "Boost.Intrusive/1.65.1@bincrafters/testing", \
-        "Boost.Move/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Unordered/1.65.1@bincrafters/testing", \
-        "Boost.Unordered/1.65.1@bincrafters/testing", \
-        "Boost.Winapi/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Container/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Date_Time/1.66.0@bincrafters/testing", \
+        "Boost.Integer/1.66.0@bincrafters/testing", \
+        "Boost.Intrusive/1.66.0@bincrafters/testing", \
+        "Boost.Move/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Unordered/1.66.0@bincrafters/testing", \
+        "Boost.Unordered/1.66.0@bincrafters/testing", \
+        "Boost.Winapi/1.66.0@bincrafters/testing"
 
     lib_short_names = ["interprocess"]
     is_header_only = True
@@ -26,9 +26,9 @@ class BoostInterprocessConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-interprocess"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
