376756bb779d9cdb37493e678540b8944ae64fff
Jerry W <jerrywiltse@gmail.com>
2018-09-29 17:15:41 -0400

    move to python_requires strategy

--- conanfile.py
+++ conanfile.py
@@ -1,74 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import python_requires
 
 
-class BoostInterprocessConan(ConanFile):
+base = python_requires("boost_base/1.67.0@bincrafters/testing")
+
+class BoostInterprocessConan(base.BoostBaseConan):
     name = "boost_interprocess"
-    version = "1.67.0"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    exports = ["LICENSE.md"]
+    url = "https://github.com/bincrafters/conan-boost_interprocess"
     lib_short_names = ["interprocess"]
-    settings = "os"
-    is_header_only = True
-
-    def package_id_additional(self):
-        self.info.header_only()
+    header_only_libs = ["interprocess"]
+    b2_requires = [
+        "boost_assert",
+        "boost_config",
+        "boost_container",
+        "boost_core",
+        "boost_date_time",
+        "boost_integer",
+        "boost_intrusive",
+        "boost_move",
+        "boost_static_assert",
+        "boost_type_traits",
+        "boost_unordered",
+        "boost_winapi"
+    ]
 
     def package_info_additional(self):
         if self.settings.os == "Linux":
-            self.cpp_info.libs.append("rt")
-
-    requires = (
-        "boost_assert/1.67.0@bincrafters/testing",
-        "boost_config/1.67.0@bincrafters/testing",
-        "boost_container/1.67.0@bincrafters/testing",
-        "boost_core/1.67.0@bincrafters/testing",
-        "boost_date_time/1.67.0@bincrafters/testing",
-        "boost_integer/1.67.0@bincrafters/testing",
-        "boost_intrusive/1.67.0@bincrafters/testing",
-        "boost_move/1.67.0@bincrafters/testing",
-        "boost_package_tools/1.67.0@bincrafters/testing",
-        "boost_static_assert/1.67.0@bincrafters/testing",
-        "boost_type_traits/1.67.0@bincrafters/testing",
-        "boost_unordered/1.67.0@bincrafters/testing",
-        "boost_winapi/1.67.0@bincrafters/testing"
-    )
-
-    # BEGIN
-
-    url = "https://github.com/bincrafters/conan-boost_interprocess"
-    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
-    license = "BSL-1.0"
-    short_paths = True
-    build_requires = "boost_generator/1.67.0@bincrafters/testing"
-
-    def package_id(self):
-        getattr(self, "package_id_additional", lambda:None)()
-
-    def source(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.source(self)
-        getattr(self, "source_additional", lambda:None)()
-
-    def build(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.build(self)
-        getattr(self, "build_additional", lambda:None)()
-
-    def package(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package(self)
-        getattr(self, "package_additional", lambda:None)()
-
-    def package_info(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package_info(self)
-        getattr(self, "package_info_additional", lambda:None)()
-
-    # END
+            self.cpp_info.libs.append("rt")    
+            
+    def package_id_additional(self):
+        self.info.header_only()
+        self.info.settings.os = str(self.settings.os)
