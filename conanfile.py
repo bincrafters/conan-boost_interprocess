15ac8515a6e7747c1229fb85cb959c6824bca331
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:05 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,29 +4,14 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostInterprocessConan(base.BoostBaseConan):
     name = "boost_interprocess"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_interprocess"
-    lib_short_names = ["interprocess"]
-    header_only_libs = ["interprocess"]
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_container",
-        "boost_core",
-        "boost_date_time",
-        "boost_integer",
-        "boost_intrusive",
-        "boost_move",
-        "boost_static_assert",
-        "boost_type_traits",
-        "boost_unordered",
-        "boost_winapi"
-    ]
+    version = "1.70.0"
 
-    def package_info_additional(self):
+    def package_info(self):
+        super(BoostInterprocessConan, self).package_info()
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("rt")
